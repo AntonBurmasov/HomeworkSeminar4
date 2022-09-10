a9634bab9a7302584be574dec44b20dc91866456
burmasovanton <looseangel1@gmail.com>
2022-09-10 23:43:04 +0500

    not working Task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int ReadNumber(string message)
+{
+Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int SummOfAllNumbers(int a)
+{
+int summ = 0;    
+
+while(a / 10 > 1)
+{
+summ = summ + (a % 10);
+}
+summ = summ + (a / 10);
+
+return summ;
+
+}
+
+int a = ReadNumber("Введите число:");
+
+int Summ = SummOfAllNumbers(a);
+Console.WriteLine($"Сумма всех цифр числа = {Summ}");
+
+
