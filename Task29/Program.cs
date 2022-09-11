26ff4ec0ad24923b2b3169a20c3cdc4a5c30d2a7
burmasovanton <looseangel1@gmail.com>
2022-09-11 19:13:49 +0500

    with improvements

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -12,21 +12,14 @@ return Convert.ToInt32(Console.ReadLine());
 }
 
 int[] array = new int[8];
+int count = 0;
+int i = 0;
 
-array[0] = ReadNumber("Введите элемент массива 1:");
-
-array[1] = ReadNumber("Введите элемент массива 2:");
-
-array[2] = ReadNumber("Введите элемент массива 3:");
-
-array[3] = ReadNumber("Введите элемент массива 4:");
-
-array[4] = ReadNumber("Введите элемент массива 5:");
-
-array[5] = ReadNumber("Введите элемент массива 6:");
-
-array[6] = ReadNumber("Введите элемент массива 7:");
-
-array[7] = ReadNumber("Введите элемент массива 8:");
+while(count < array.Length)
+{
+array[i] = ReadNumber("Введите элемент массива:");
+i++;
+count++;
+}
 
 Console.WriteLine(string.Join(", ", array));
